3fc16e38638b29e30a34d340a415cc803ac59b2e
Krishnendu Sengupta <k.sengupta.29.11@gmail.com>
2023-08-09 21:17:01 +0530

    testing LLM

--- autogptpl.py
+++ autogptpl.py
@@ -1,6 +1,6 @@
 from transformers import AutoTokenizer, pipeline, logging, TextGenerationPipeline
 from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
-import argparse
+import json
 
 model_name_or_path = "TheBloke/WizardCoder-15B-1.0-GPTQ"
 # Or to load it locally, pass the local download path
@@ -21,28 +21,23 @@ logging.set_verbosity(logging.CRITICAL)
 
 pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, return_full_text=False)
 
-def analys(tmpl: str, pmt: str):
-    prompt_template = tmpl
-    prompt = prompt_template.format(prompt=pmt)
+f = open('prompt_constants.json')
+pc = json.load(f)
+# prompt_template = (pc['templates'][0])
 
-    outputs = pipe(prompt, max_new_tokens=512, do_sample=True, temperature=0.2, top_k=50, top_p=0.95)
 
-    print(outputs[0])
+def analys(services: [str], prompt_template:str, pmt: str):
+    print("H2")
+    services = ", ".join(services)
+    prompt = prompt_template.format(services=services, prompt=pmt)
+    print(prompt)
+    outputs = pipe(prompt, max_new_tokens=512, do_sample=True, temperature=0.1, top_k=50, top_p=0.95)
     return outputs[0]['generated_text']
 
 # Prompt Template
 # ---------------
-# You are given a block of logs from a running process. Identify the severity of the logs, which may be none, low, medium, high or critical. Also provide actionable insights, if the severity is not low, explain why you gave the severity in here. Also provide a review of the logs, from the viewpoint of a compliance monitoring service and software log analyzer.
-# You should only give the response in JSON.
-#
-# SERVICE: services[].join(',') (like "Docker,PostgreSQL")
-#
-# ### LOGS:
-# {prompt}
-#
-# ### RESPONSE:
+
 
 # Prompt
 # ------
 # ~Logs straight away~
-
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import collections
+import time
 from typing import Iterator
 
 import pb.analyzer_pb2 as analyzer_pb2
@@ -7,16 +7,21 @@ import pb.analyzer_pb2_grpc as grpc_stub
 import grpc
 from concurrent import futures
 from grpc_reflection.v1alpha import reflection
-from google.protobuf.json_format import MessageToJson, ParseDict
 # from archive.llmc import run_llm
-from archive.autogptpl import analys
+from autogptpl import analys
 
 class AnalyzerServicer(grpc_stub.AnalyzerServicer):
     def __init__(self):
         pass
 
     def llmPrompt(self, request: analyzer_pb2.Prompt, context):
-        return analyzer_pb2.Prompt(pmt=analys(request.tmp, request.pmt))
+        start = time.time()
+        print("H1")
+        res = analys(services=["Docker", "PostgreSQL"], prompt_template=request.tmp, pmt=request.pmt)
+        end = time.time()
+        diff = end - start
+        print("Time Taken: {} s".format(diff), res, end='\n')
+        return analyzer_pb2.Prompt(pmt=res)
 
     def analyzeLog(self, request_iterator: Iterator[analyzer_pb2.AnalyzerRequest], context):
         for req in request_iterator:
